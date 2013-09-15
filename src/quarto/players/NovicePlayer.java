38efbcc6e443ea29836e87b87b6567a4efcf40e0
Dominik Horb <dom2503@aol.com>
2013-09-15 16:05:55 +0200

    Changes to NovicePlayer

--- src/quarto/players/NovicePlayer.java
+++ src/quarto/players/NovicePlayer.java
@@ -6,76 +6,72 @@ import quarto.Board;
 import quarto.Piece;
 
 /**
- * This player makes it moves according to these rules (taken from 
- * the task document):
- * 
- * It always places its piece in a winning formation, if possible. 
- * Otherwise, it chooses randomly among the existing open cells.
- * 
- * If given a choice among pieces to give the opponent, it always chooses 
- * one that cannot be used to immediately win the game, if such a piece 
- * is available.
+ * This player makes it moves according to these rules (taken from the task document):
+ *
+ * It always places its piece in a winning formation, if possible. Otherwise, it chooses randomly
+ * among the existing open cells.
+ *
+ * If given a choice among pieces to give the opponent, it always chooses one that cannot be used to
+ * immediately win the game, if such a piece is available.
  */
 public class NovicePlayer extends QuartoPlayer {
 
-    public final static int BOARD_LENGTH = 4;
-    private ArrayList<Piece> pieces;
-    private RandomPlayer random;
+  public final static int BOARD_LENGTH = 4;
+  private RandomPlayer random;
 
-    public NovicePlayer(Board board) {
-        super(board);
-        random = new RandomPlayer(this.getBoard());
+  public NovicePlayer(Board board) {
+    super(board);
+    random = new RandomPlayer(board);
+  }
 
-    }
-
-    @Override
-    public String makeMove() {
-        boolean moveMade = false;
-
-        for (int i = 0; i < BOARD_LENGTH; i++) {
-            for (int j = 0; j < BOARD_LENGTH; j++) {
-                Board newBoard = new Board(this.getBoard());
-
-
-                if (newBoard.setField(i, j, this.getGivenPiece()) && newBoard.gameWasWon() && !moveMade) {
-                    this.getBoard().setField(i, j, this.getGivenPiece());
-                    moveMade = true;
-                    return "I made my move to " + (i + 1) + (char) (j + 65);
-                }
-            }
-        }
-        if (!moveMade) {
-            random.makeMove();
-            this.setGivenPiece(null);
-        }
-        return "";
-    }
-
-    @Override
-    public Piece selectPieceForOpponent() {
-        this.pieces = new ArrayList<Piece>(this.getBoard().getLeftoverPieces());
-        Iterator<Piece> it = pieces.iterator();
-
-        while (it.hasNext()) {
-            Piece piece = it.next();
-
-            for (int i = 0; i < BOARD_LENGTH; i++) {
-                for (int j = 0; j < BOARD_LENGTH; j++) {
+  @Override
+  public void setGivenPiece(Piece piece) {
+    super.setGivenPiece(piece);
+    this.random.setGivenPiece(piece);
+  }
+  
+  @Override
+  public String makeMove() {
+    for (int i = 0; i < BOARD_LENGTH; i++) {
+      for (int j = 0; j < BOARD_LENGTH; j++) {
         Board newBoard = new Board(this.getBoard());
 
-                    if (newBoard.setField(i, j, piece) && !newBoard.gameWasWon() ) {
-                        piece = this.getBoard().takePieceForOpponent(piece);
-                        return piece;
-                    }
-                }
-            }
-
-
-
-
-
+        if (newBoard.setField(i, j, this.getGivenPiece()) && newBoard.gameWasWon()) {
+          this.getBoard().setField(i, j, this.getGivenPiece());
+          return "I made my move to " + (i + 1) + (char) (j + 65);
         }
-        return random.selectPieceForOpponent();
+      }
+    }
+    String result = random.makeMove();
+    this.setGivenPiece(null);
+    return result;
+  }
+
+  @Override
+  public Piece selectPieceForOpponent() {
+    ArrayList<Piece> pieces = this.getBoard().getLeftoverPieces();
+    Iterator<Piece> it = pieces.iterator();
+
+    while (it.hasNext()) {
+      Piece piece = it.next();
+
+      for (int i = 0; i < BOARD_LENGTH; i++) {
+        for (int j = 0; j < BOARD_LENGTH; j++) {
+          Board newBoard = new Board(this.getBoard());
+
+          if (newBoard.setField(i, j, piece) && !newBoard.gameWasWon()) {
+            piece = this.getBoard().takePieceForOpponent(piece);
+            return piece;
+          }
+        }
+      }
+
+
+
+
 
     }
+    return random.selectPieceForOpponent();
+
+  }
 }
